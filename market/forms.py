cfa04d754cd5338e13284cbc317563663d79aa40
r3X <rch246chauhan@gmail.com>
2022-06-15 19:31:50 +0530

    Commit

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,16 @@
 from tokenize import String
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired
+
 
 class RegisterForm(FlaskForm):
-    username=StringField(label="Username")
-    email_address=StringField(label="Email address")
-    password1=PasswordField(label="Password")
-    password2=PasswordField(label="Confirm Password")
-    submit=SubmitField(label="Create Account")
+    username = StringField(label="Username", validators=[
+                           Length(min=5), DataRequired()])
+    email_address = StringField(label="Email address", validators=[
+                                Email(), DataRequired()])
+    password1 = PasswordField(label="Password", validators=[
+                              Length(min=6), DataRequired()])
+    password2 = PasswordField(label="Confirm Password",
+                              validators=[EqualTo("password1"), DataRequired()])
+    submit = SubmitField(label="Create Account")
